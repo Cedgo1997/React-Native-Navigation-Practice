ef2bd6bad9edff705e5dacc5ae07ac5aa1cfaecf
César Gonzalez <cedgo1997@gmail.com>
2021-08-26 00:48:50 -0400

    Setting up application

---
+++ App.tsx
@@ -0,0 +1,12 @@
+/* eslint-disable prettier/prettier */
+import * as React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { Text } from 'react-native';
+
+export const App = () => {
+  return (
+    <NavigationContainer>
+      <Text>Hello, World</Text>
+    </NavigationContainer>
+  );
+}
