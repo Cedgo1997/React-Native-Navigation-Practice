d7829d31bb4d608bf280a4b0eb6ff49e36a155aa
César Gonzalez <cedgo1997@gmail.com>
2021-08-26 20:21:40 -0400

    using hook or props to navigate

--- src/navigator/StackNavigator.tsx
+++ src/navigator/StackNavigator.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable prettier/prettier */
 import React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
 import { Pagina1Screen } from '../screens/Pagina1Screen';
@@ -8,10 +9,17 @@ const Stack = createStackNavigator();
 
 export const StackNavigator = () => {
     return (
-        <Stack.Navigator>
-            <Stack.Screen name="Pagina1Screen" component={Pagina1Screen} />
-            <Stack.Screen name="Pagina2Screen" component={Pagina2Screen} />
-            <Stack.Screen name="Pagina3Screen" component={Pagina3Screen} />
+        <Stack.Navigator screenOptions={{
+            headerStyle:{
+                elevation: 0,
+            },
+            cardStyle: {
+                backgroundColor: 'white',
+            },
+        }}>
+            <Stack.Screen name="Pagina1Screen" options={{title: "Página 1"}} component={Pagina1Screen} />
+            <Stack.Screen name="Pagina2Screen" options={{title: "Página 2"}} component={Pagina2Screen} />
+            <Stack.Screen name="Pagina3Screen" options={{title: "Página 3"}} component={Pagina3Screen} />
         </Stack.Navigator>
     );
 }
