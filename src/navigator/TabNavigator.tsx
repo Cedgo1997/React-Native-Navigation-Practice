47fdf6a090309e8d46bdbd5f5f6c3f6d2213c7b2
César Gonzalez <cedgo1997@gmail.com>
2021-08-27 02:24:27 -0400

    styling tabs

--- src/navigator/TabNavigator.tsx
+++ src/navigator/TabNavigator.tsx
@@ -3,15 +3,25 @@ import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { Tab1Screen } from '../screens/Tab1Screen';
 import { Tab2Screen } from '../screens/Tab2Screen';
 import { Tab3Screen } from '../screens/Tab3Screen';
+import { StackNavigator } from './StackNavigator';
+import { colors } from '../theme/appTheme';
 
 const Tab = createBottomTabNavigator();
 
 export const TabNavigator = () => {
     return (
-        <Tab.Navigator>
-            <Tab.Screen name="Tab1Screen" component={Tab1Screen} />
-            <Tab.Screen name="Tab2Screen" component={Tab2Screen} />
-            <Tab.Screen name="Tab3Screen" component={Tab3Screen} />
+        <Tab.Navigator
+        sceneContainerStyle={{
+            backgroundColor: 'white'
+        }}
+            screenOptions={
+                () => ({
+                    tabBarActiveTintColor: colors.primary,
+                })
+            }>
+            <Tab.Screen options={{title: 'Tab 1'}} name="Tab1Screen" component={Tab1Screen} />
+            <Tab.Screen options={{title: 'Tab 2'}} name="Tab2Screen" component={Tab2Screen} />
+            <Tab.Screen options={{title: 'Stack'}} name="StackNavigator" component={StackNavigator} />
         </Tab.Navigator>
     );
 }
--- src/theme/appTheme.tsx
+++ src/theme/appTheme.tsx
@@ -1,6 +1,11 @@
 /* eslint-disable prettier/prettier */
 import { StyleSheet } from 'react-native';
 
+export const colors = {
+    primary: '#5856D6',
+
+};
+
 export const styles = StyleSheet.create({
     globalMargin: {
         marginHorizontal: 20,
