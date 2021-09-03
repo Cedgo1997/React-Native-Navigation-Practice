78936592061b580747b457f76d3386aea8264fd0
cedgo <cedgo1997@gmail.com>
2021-09-03 00:23:30 -0400

    Adding icons

--- src/navigator/TopTabNavigator.tsx
+++ src/navigator/TopTabNavigator.tsx
@@ -1,15 +1,52 @@
 /* eslint-disable prettier/prettier */
+/* eslint-disable react-native/no-inline-styles */
 import React from 'react';
 import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
 import { ChatScreen } from '../screens/ChatScreen';
 import { ContactsScreen } from '../screens/ContactsScreen';
 import { AlbumScreen } from '../screens/AlbumScreen';
+import { useSafeAreaInsets } from 'react-native-safe-area-context';
+import { colors } from '../theme/appTheme';
+import { Text } from 'react-native';
 
 const Tab = createMaterialTopTabNavigator();
 
 export const TopTabNavigator = () => {
+
+  const { top: paddingTop } = useSafeAreaInsets();
+
   return (
-    <Tab.Navigator>
+    <Tab.Navigator
+      sceneContainerStyle={{
+        backgroundColor: 'white',
+      }}
+      screenOptions={({ route }) => ({
+        tabBarPressColor: colors.primary,
+        tabBarShowIcon: true,
+        tabBarIndicatorStyle: {
+          backgroundColor: colors.primary,
+        },
+        tabBarStyle: {
+          shadowColor: 'transparent',
+          elevation: 0,
+        },
+        tabBarIcon: ({ color }) => {
+          let iconName: string = '';
+          switch (route.name) {
+            case 'ChatScreen':
+              iconName = 'CH';
+              break;
+            case 'ContactsScreen':
+              iconName = 'CT';
+              break;
+            case 'AlbumScreen':
+              iconName = 'AL';
+              break;
+          }
+          return (<Text style={{ color }}>{iconName}</Text>);
+        }
+      })}
+      style={{ paddingTop }}>
       <Tab.Screen name="ChatScreen" component={ChatScreen} />
       <Tab.Screen name="ContactsScreen" component={ContactsScreen} />
       <Tab.Screen name="AlbumScreen" component={AlbumScreen} />
--- src/screens/Tab1Screen.tsx
+++ src/screens/Tab1Screen.tsx
@@ -1,10 +1,15 @@
-import React, { useEffect } from 'react';
+/* eslint-disable prettier/prettier */
+import React from 'react';
 import { Text, View } from 'react-native';
+import { styles } from '../theme/appTheme';
+import Icon from 'react-native-vector-icons/Ionicons';
 
 export const Tab1Screen = () => {
     return (
-        <View>
-            <Text>Tab1Screen</Text>
+        <View style={styles.globalMargin}>
+            <Text style={styles.title}>Icons</Text>
+            <Text><Icon name="airplane-outline" size={30} color="#900" /></Text>
+
         </View>
     );
 };
