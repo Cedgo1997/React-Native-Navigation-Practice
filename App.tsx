7a138fbcec7bc87ce5d42ab1c1a4e599e88dfde9
cedgo <cedgo1997@gmail.com>
2021-09-06 19:52:29 -0400

    Create and use context

--- App.tsx
+++ App.tsx
@@ -3,12 +3,22 @@ import * as React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 /* import { DrawerNavigator } from './src/navigator/DrawerNavigator'; */
 import { DrawerCustom } from './src/navigator/DrawerCustom';
+import { AuthProvider } from './src/context/AuthContext';
 
 export const App = () => {
   return (
     <NavigationContainer>
-      <DrawerCustom />
+      <AppState>
+        <DrawerCustom />
+      </AppState>
     </NavigationContainer>
   );
 };
 
+const AppState = ({ children }: any) => {
+  return (
+    <AuthProvider>
+      {children}
+    </AuthProvider>
+  );
+};
