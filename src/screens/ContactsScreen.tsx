469f1a8150b3623f07ade3da5fa4ec0015cc6cf6
cedgo <cedgo1997@gmail.com>
2021-09-06 23:03:18 -0400

    Dispatching actions

--- src/screens/ContactsScreen.tsx
+++ src/screens/ContactsScreen.tsx
@@ -1,11 +1,17 @@
 /* eslint-disable prettier/prettier */
-import React from 'react'
-import { Text, View } from 'react-native'
+import React, { useContext } from 'react';
+import { Button, Text, View } from 'react-native';
+import { AuthContext } from '../context/AuthContext';
 
 export const ContactsScreen = () => {
+
+    const { authState: { isLoggedIn }, signIn } = useContext(AuthContext);
+
+
     return (
         <View>
             <Text>Contacts Screen</Text>
+            {!isLoggedIn && (<Button onPress={signIn} title="SignIn" />)}
         </View>
     );
 };
